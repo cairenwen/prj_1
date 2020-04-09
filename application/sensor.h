41b9957cd216ff2e63c92c3b650caf6b96f2151b
acre <acre@qq.com>
2020-04-09 16:55:58 +0800

    Revert "Revert "project create for sensor read""
    This reverts commit dfa96d6dc3834bd2d6673adc14227f6528a641a9.

---
+++ application/sensor.h
@@ -0,0 +1,6 @@
+#ifndef __SENSOR_H_
+#define __SENSOR_H_
+
+int sensor_get(void);
+
+#endif
---
+++ bsp/gpio.c
@@ -0,0 +1,7 @@
+#include "gpio.h"
+#include "stdio.h"
+void gpio_init(void)
+{
+	printf("gpio all init!");
+
+}
